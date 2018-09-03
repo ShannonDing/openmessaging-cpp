a46644d6d8b127db5d6eea06b8d3c9c5f290f695
shannon.dl <shannon.dl@alibaba-inc.com>
2018-09-03 23:14:32 +0800

    OpenMessaging1.0 C++ API[add copyright]

--- OMSResponseStatus.h
+++ OMSResponseStatus.h
@@ -1,11 +1,23 @@
-//
-// Created by Shannon on 2018/9/2.
-//
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef OMS_RESPONSE_STATUS_H
 #define OMS_RESPONSE_STATUS_H
 BEGIN_NAMESPACE_u2(io, openmessaging)
-enum Error{
+enum OMSResponceStatus{
     UnsupportedVersion = 1101,
     Success = 1200,
     BadRequest = 1400,
